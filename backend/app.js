ad615eeb635f9327a845c2a8899022e1855cde98
assyris <assyris@hotmail.fr>
2021-09-30 15:43:28 +0200

    base de donnée

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing')
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://assyris:Monkey69@cluster0.swnqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('requête reçu');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+  next();  
 });
 
-app.use((req, res, next) => {
-    res.json({message: 'votre blablalal'});
-    next();
-});
-
-app.use((req, res) => {
-    console.log('reponse envoyé avec succé');
+app.use('/api/stuff', (req, res) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
