485b4d47367cdddcb921b89004aa4e6a7b919ed4
assyris <assyris@hotmail.fr>
2021-10-06 15:33:05 +0200

    rmplace frontend ajout route controller backend

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const Thing = require('./models/thing')
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://assyris:Monkey69@cluster0.swnqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect('mongodb+srv://assyris:Monkey69@cluster0.swnqk.mongodb.net/blabla?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-  next();  
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/stuff', (req, res) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/sauces', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
